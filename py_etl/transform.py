0d399e1dbc18351e92442fe9219026513425dc19
Max Kavaliou <mkovaliou@gmail.com>
2025-05-30 23:48:44 +0300

    refactor: update transform.py to respect column types

--- py_etl/transform.py
+++ py_etl/transform.py
@@ -4,6 +4,7 @@ import pandas as pd
 
 SELECT_COLUMNS = ["collision_id", "crash_date", "latitude", "longitude"]
 RENAME_COLUMNS = {"collision_id": "crash_id"}
+TYPE_COLUMNS = {"crash_id": "int32", "latitude": "float", "longitude": "float"}
 
 def transform_crashes_data(df: pd.DataFrame) -> pd.DataFrame:
     df["crash_date"] = pd.to_datetime(df["crash_date"]).combine(
@@ -12,5 +13,6 @@ def transform_crashes_data(df: pd.DataFrame) -> pd.DataFrame:
 
     df = df.loc[:, SELECT_COLUMNS]
     df = df.rename(columns=RENAME_COLUMNS)
+    df = df.astype(TYPE_COLUMNS)
 
     return df
